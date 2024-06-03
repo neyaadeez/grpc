4acfd6222bab5a0e01db12195245793dba819451
Mustafa Mokashi <neyaadeez@gmail.com>
2024-06-03 12:50:07 -0700

    server streaming

--- server/greet.go
+++ server/greet.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	proto "github.com/neyaadeez/grpc/proto"
@@ -12,4 +13,16 @@ func (s *Server) Greet(conx context.Context, in *proto.GreetRequest) (*proto.Gre
 	return &proto.GreetResponse{
 		Result: "Hello " + in.FirstName,
 	}, nil
+}
+
+func (s *Server) GreetMany(in *proto.GreetRequest, stream proto.GreetService_GreetManyServer) error {
+	log.Printf("GreetMany function was invoked with parameter: %v\n", in)
+
+	for i := 0; i < 10; i++ {
+		stream.Send(&proto.GreetResponse{
+			Result: fmt.Sprintf("Hello %s, this is steaming test: %d\n", in.FirstName, i),
+		})
+	}
+
+	return nil
 }
